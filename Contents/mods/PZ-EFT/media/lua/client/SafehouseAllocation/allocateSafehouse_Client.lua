68773718a8d059771f256918721a680578a0b691
Kim <kim.scicluna@live.com>
2023-06-16 21:57:26 +0200

    Add comment

--- Contents/mods/PZ-EFT/media/lua/client/SafehouseAllocation/allocateSafehouse_Client.lua
+++ Contents/mods/PZ-EFT/media/lua/client/SafehouseAllocation/allocateSafehouse_Client.lua
@@ -2,7 +2,8 @@ local function OnCreatePlayer(playerIndex, player)
 	if player == getPlayer() then
         --On join, request safehouse allocation data
         print("On Create Player, RequestSafehouseAllocation");
-        --Teleport player to hub
+        --Teleport player to hub 
+        -- TODO: maybe change coordinates
         PZEFT_UTILS.TeleportPlayer(player,302,302,0);
 
         --Request safe house allocation, which in turn will teleport the player to the assigned safehouse
