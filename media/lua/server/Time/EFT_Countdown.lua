9c3b66030f4e780cd54325b467f03f0ba6ae9428
Pao <picchetti1998@gmail.com>
2023-06-21 19:03:50 +0200

    Readded comment to EFT_Timer and EFT_Countdown

--- media/lua/server/Time/EFT_Countdown.lua
+++ media/lua/server/Time/EFT_Countdown.lua
@@ -1,3 +1,12 @@
+--[[⠀
+Based on lua_timers by Vyshnia!
+Permissions granted by original author
+
+Original mod details:
+Workshop ID: 2875394066
+Mod ID: LuaTimers
+--]]
+
 local os_time = os.time
 local EFT_Countdown = {}
 
