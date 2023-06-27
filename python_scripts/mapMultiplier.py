005139c08d52faeb3ba13e1316d27431ec05c5ea
Pao <picchetti1998@gmail.com>
2023-06-27 13:07:24 +0200

    Fixing some stuff in the script

--- python_scripts/mapMultiplier.py
+++ python_scripts/mapMultiplier.py
@@ -1,15 +1,17 @@
 import shutil
 import os
 
+buffer = 1
+
 # TODO placeholders
 start_x = 1
 start_y = 1
 
-max_x = 100
-max_y = 100
+max_x = 7
+max_y = 7
 
-inc_x = 10
-inc_y = 20
+inc_x = 2
+inc_y = 3
 
 cwd = os.getcwd()
 
@@ -27,15 +29,17 @@ def copyLoop(base_string):
     curr_x = 0
     curr_y = 0
 
-    for x in range(start_x + inc_x, max_x, inc_x):
-        for y in range(start_y + inc_y, max_y, inc_y):
+    for x in range(start_x, max_x, inc_x):
+        curr_x = curr_x + x + buffer
 
-            curr_x = curr_x + x
-            curr_y = curr_y + y
+        for y in range(start_y, max_y, inc_y):
+
+            curr_y = curr_y + y + buffer
             curr_file = base_string.format(x=curr_x, y=curr_y)
             curr_file_path = os.path.join(output_dir, curr_file)
-            
+
             shutil.copyfile(original_file_path, curr_file_path)
+
 
 ############################
 base_lotheader = "{x}_{y}.lotheader"
