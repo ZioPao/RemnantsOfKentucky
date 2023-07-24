b7fc66c9720c6e35e893416a71b048aa44c0840f
Pao <picchetti1998@gmail.com>
2023-07-24 11:50:54 +0200

    Switched to icons for the exit info

--- media/lua/client/EFTUI/DuringMatch/MapHandler.lua
+++ media/lua/client/EFTUI/DuringMatch/MapHandler.lua
@@ -1,9 +1,9 @@
--- TODO Base it on ISWorldMapSymbols
 -- TODO When in a match, the player should be able to check out the map to see where extraction points are located
 -- TODO We need SymbolsAPI, a way to automatically write at the extraction points location, a way to clear everything at match startup, etc.
 
+
 -- TODO Make it local
-EFTMapHandler = ISWorldMapSymbolTool:derive("EFTMapHandler")
+EFTMapHandler = {}
 
 function EFTMapHandler:new(symbolsAPI)
 	local o = {}
@@ -14,41 +14,39 @@ function EFTMapHandler:new(symbolsAPI)
 	return o
 end
 
-function EFTMapHandler:activate()
-    self:write()
-
-end
-
 function EFTMapHandler:write()
-    -- TODO Should be triggered when a match is starting
 
     -- TODO Get extraction points list
-    local extractionPoints = {{x=1, y=1}, {x=2, y=4}}
+    local extractionPoints = {}
+
+    table.insert(extractionPoints, {x=ZombRand(-500, 500), y=ZombRand(-500, 500)})
+    table.insert(extractionPoints, {x=ZombRand(-500, 500), y=ZombRand(-500, 500)})
+    table.insert(extractionPoints, {x=ZombRand(-500, 500), y=ZombRand(-500, 500)})
+    table.insert(extractionPoints, {x=ZombRand(-500, 500), y=ZombRand(-500, 500)})
+    table.insert(extractionPoints, {x=ZombRand(-500, 500), y=ZombRand(-500, 500)})
+    table.insert(extractionPoints, {x=ZombRand(-500, 500), y=ZombRand(-500, 500)})
+
 
     --TODO Get relative coordinates based on the instance id
-    local instanceCoords = {x=1, y=1}
+    local instanceCoords = {x=getPlayer():getX(), y=getPlayer():getY()}     -- TODO Only for test
 
-    -- TODO Loop through extraction points and add the note on the map
-
+    --Loop through extraction points and add the note on the map
     for i=1, #extractionPoints do
-        print("EFT Map handler writing")
-        local textString = "test " .. tostring(i)
-
 		local singleExtractionPoint = extractionPoints[i]
-        
         local x = instanceCoords.x + singleExtractionPoint.x
         local y = instanceCoords.y + singleExtractionPoint.y
 
-        local textSymbol = self.symbolsAPI:addUntranslatedText(textString, UIFont.Handwritten, x, y)
-		textSymbol:setRGBA(1, 1, 1, 1.0)
+        -- TODO Maybe use an icon?
+        local textSymbol = self.symbolsAPI:addTexture("PZEFT-Exit", x, y)
+		textSymbol:setRGBA(0, 0, 0, 1.0)
 		textSymbol:setAnchor(0.0, 0.0)
 		textSymbol:setScale(ISMap.SCALE)
     end
 
 end
 
-function EFTMapHandler:clean()
-	-- TODO Clean everything else. Must be triggered when a player enters the game (?)
+function EFTMapHandler:clear()
+    self.symbolsAPI:clear()
 end
 
 function EFTMapHandler:deactivate()
@@ -65,6 +63,9 @@ local og_ISWorldMapInstantiate = ISWorldMap.instantiate
 function ISWorldMap:instantiate()
     print("Instantiating ISWorldMap")
     og_ISWorldMapInstantiate(self)
+
+    -- TODO Should be triggered when a match is starting?
     self.eftMapHandler = EFTMapHandler:new(self.mapAPI:getSymbolsAPI())
+    self.eftMapHandler:clear()
     self.eftMapHandler:write()
 end
---
+++ media/lua/shared/PZ_EFT_MapDefinitions.lua
@@ -0,0 +1,1 @@
+MapSymbolDefinitions.getInstance():addTexture("PZEFT-Exit", "media/textures/ExitIcon.png")
