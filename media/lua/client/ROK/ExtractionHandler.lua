581f4aead00a9ae2608f420e2972ca4ab96b2050
ZioPao <picchetti1998@gmail.com>
2023-12-11 03:16:17 +0100

    Simplified ExtractionHandler logic

--- media/lua/client/ROK/ExtractionHandler.lua
+++ media/lua/client/ROK/ExtractionHandler.lua
@@ -1,14 +1,28 @@
 require "ROK/ClientData"
 local ExtractionPanel = require("ROK/UI/DuringMatch/ExtractionPanel")
 local ClientState = require("ROK/ClientState")
+LuaEventManager.AddEvent("PZEFT_UpdateExtractionZoneState")
 local os_time = os.time
 ------------------
 
-LuaEventManager.AddEvent("PZEFT_UpdateExtractionZoneState")
+---@class ExtractionHandler
+---@field key string
+local ExtractionHandler = {}
 
-local function ExtractionUpdateEvent()
-    if ClientState.isInRaid == false then return end
+---Starts the loop to handle the event
+---@param isInRaid boolean
+function ExtractionHandler.ToggleEvent(isInRaid)
+    if isInRaid == true then
+        Events.OnTick.Add(ExtractionHandler.RunEvent)
+    else
+        Events.OnTick.Remove(ExtractionHandler.RunEvent)
+    end
 
+end
+Events.PZEFT_UpdateClientStatus.Add(ExtractionHandler.ToggleEvent)
+
+---Triggers PZEFT_UpdateExtractionZoneState if player is in an extraction zone
+function ExtractionHandler.RunEvent()
     local pl = getPlayer()
     local currentInstanceData = getPlayer():getModData().currentInstance
     if currentInstanceData == nil or currentInstanceData.id == nil then return end
@@ -25,18 +39,27 @@ local function ExtractionUpdateEvent()
     end
 end
 
-Events.EveryOneMinute.Add(ExtractionUpdateEvent)
+---Triggered when a player enters an extraction area
+---@param key string
+---@private
+function ExtractionHandler.ManageEvent(key)
+    if ExtractionHandler.key and ExtractionHandler.key ~= key then return end
 
--------------------------------------------------
+    if ClientState.extractionStatus[key] then
+        ExtractionHandler.key = key
+        ExtractionPanel.Open()
+    else
+        ExtractionHandler.key = nil
+        ExtractionPanel.Close()
+    end
+end
+Events.PZEFT_UpdateExtractionZoneState.Add(ExtractionHandler.ManageEvent)
 
----@class ExtractionHandler
----@field key string
-local ExtractionHandler = {}
 
 function ExtractionHandler.HandleTimer()
     local cTime = os_time()
     --print(cTime)
-    debugPrint(ClientState.extractionStatus[ExtractionHandler.key])
+    --debugPrint(ClientState.extractionStatus[ExtractionHandler.key])
     if ClientState.extractionStatus[ExtractionHandler.key] == nil then
         Events.OnTick.Remove(ExtractionHandler.HandleTimer)
         debugPrint("Player not in extraction zone anymore")
@@ -61,22 +84,6 @@ function ExtractionHandler.DoExtraction()
     Events.OnTick.Add(ExtractionHandler.HandleTimer)
 end
 
----Triggered when a player enters an extraction area
----@param key string
----@private
-function ExtractionHandler.ManageEvent(key)
-    if ExtractionHandler.key and ExtractionHandler.key ~= key then return end
-
-    if ClientState.extractionStatus[key] then
-        ExtractionHandler.key = key
-        ExtractionPanel.Open()
-    else
-        ExtractionHandler.key = nil
-        ExtractionPanel.Close()
-    end
-end
-
-Events.PZEFT_UpdateExtractionZoneState.Add(ExtractionHandler.ManageEvent)
 
 
 return ExtractionHandler
