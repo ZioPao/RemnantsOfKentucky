f9edbae216ff21e3935379bf262e946add778940
ZioPao <picchetti1998@gmail.com>
2024-01-12 17:35:20 +0100

    Added override for boredom and unhappyness

---
+++ media/lua/client/ROK/OverrideStats.lua
@@ -0,0 +1,7 @@
+local function ResetBoredomAndUnhappyness()
+    local bd = getPlayer():getBodyDamage()
+    bd:setBoredomLevel(0)
+    bd:setUnhappynessLevel(0)
+end
+
+Events.EveryOneMinute.Add(ResetBoredomAndUnhappyness)
