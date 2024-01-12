c88884f69bfcb65f88260a94589bcf3fe3a34ea6
ZioPao <picchetti1998@gmail.com>
2024-01-12 17:34:50 +0100

    Added placeholder for traits

---
+++ media/lua/shared/ROK/Traits.lua
@@ -0,0 +1,14 @@
+
+local function InitializeTraits()
+
+    --* Positive traits
+    --local bigPockets = TraitFactory.addTrait("test", "123", -4, "123", false, false)
+    --local test2 = TraitFactory.addTrait("test", "123", -4, "123", false, false)
+
+    -- TODO Modded Organized, add cells
+    -- TODO Modded disorganized
+end
+
+
+
+Events.OnGameBoot.Add(InitializeTraits)
