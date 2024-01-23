f5e8aa76a8edab5c0a43858ec87cadc9f5cf6b93
ZioPao <picchetti1998@gmail.com>
2024-01-23 19:49:39 +0100

    Added failsafe in ZombieDespawner

--- media/lua/client/ROK/ZombieDespawner.lua
+++ media/lua/client/ROK/ZombieDespawner.lua
@@ -1,6 +1,10 @@
 -- Should be triggered only when in a safehouse, never elsewhere
+local SafehouseInstanceHandler = require("ROK/SafehouseInstanceHandler")
 
 local function DespawnZombies(zombie)
+    -- Double check, player need to be in their safehouse
+    if not SafehouseInstanceHandler.IsInSafehouse() then return end
+
     local id = zombie:getOnlineID()
     debugPrint("Found a zombie in a safehouse! id = " .. tostring(id))
     --sendClientCommand(EFT_MODULES.Safehouse, "DespawnZombies", { id = id })
