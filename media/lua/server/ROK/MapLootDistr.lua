009806c6fca6f2d63ab10fa942410a6efb640d68
ZioPao <picchetti1998@gmail.com>
2024-02-25 19:38:43 +0100

    Test for loot distributions

---
+++ media/lua/server/ROK/MapLootDistr.lua
@@ -0,0 +1,115 @@
+require('Items/Distributions')
+require('Items/SuburbsDistributions')
+
+
+SuburbsDistributions = SuburbsDistributions or {}
+
+SuburbsDistributions.armystorage.crate = {
+            procedural = true,
+            procList = {
+                {name="ArmyStorageGuns", min=1, max=4, weightChance=60},
+                {name="ArmyStorageGuns", min=1, max=2, weightChance=40},
+                {name="ArmyStorageGuns", min=2, max=4, weightChance=60},
+                {name="ArmyStorageGuns", min=1, max=4, weightChance=60},
+                {name="ArmyStorageGuns", min=0, max=99, weightChance=20},
+                {name="ArmyStorageGuns", min=1, max=4, weightChance=60},
+                {name="ArmyStorageMedical", min=1, max=4, weightChance=60},
+                {name="ArmyStorageOutfit", min=1, max=4, weightChance=60},
+                {name="ArmySurplusFootwear", min=1, max=2, weightChance=60},
+                {name="ArmySurplusMisc", min=1, max=2, weightChance=60},
+                {name="ArmySurplusMisc", min=0, max=1, weightChance=20},
+                {name="ArmyStorageGuns", min=1, max=2, weightChance=60},
+                {name="ArmyStorageGuns", min=1, max=2, weightChance=80},
+                {name="ArmyStorageGuns", min=0, max=2, weightChance=40},
+                {name="ArmyStorageElectronics", min=1, max=4, weightChance=60},
+                {name="ArmyStorageElectronics", min=0, max=2, weightChance=40},
+            }
+}
+
+SuburbsDistributions.armystorage.metal_shelves = {
+            procedural = true,
+            procList = {
+                {name="ArmyStorageGuns", min=1, max=4, weightChance=60},
+                {name="ArmyStorageGuns", min=1, max=2, weightChance=40},
+                {name="ArmyStorageGuns", min=2, max=4, weightChance=60},
+                {name="ArmyStorageGuns", min=1, max=4, weightChance=60},
+                {name="ArmyStorageGuns", min=0, max=99, weightChance=20},
+                {name="ArmyStorageGuns", min=1, max=4, weightChance=60},
+                {name="ArmyStorageMedical", min=1, max=4, weightChance=60},
+                {name="ArmyStorageOutfit", min=1, max=4, weightChance=60},
+                {name="ArmySurplusFootwear", min=1, max=2, weightChance=60},
+                {name="ArmySurplusMisc", min=1, max=2, weightChance=60},
+                {name="ArmySurplusMisc", min=0, max=1, weightChance=20},
+                {name="ArmyStorageGuns", min=1, max=2, weightChance=60},
+                {name="ArmyStorageGuns", min=1, max=2, weightChance=80},
+                {name="ArmyStorageGuns", min=0, max=2, weightChance=40},
+                {name="ArmyStorageElectronics", min=1, max=4, weightChance=60},
+                {name="ArmyStorageElectronics", min=0, max=2, weightChance=40},
+            }
+}
+
+SuburbsDistributions.armystorage.freezer = {
+            procedural = true,
+            procList = {
+                {name="ArmyStorageGuns", min=1, max=4, weightChance=60},
+                {name="ArmyStorageGuns", min=1, max=2, weightChance=40},
+                {name="ArmyStorageGuns", min=2, max=4, weightChance=60},
+                {name="ArmyStorageGuns", min=1, max=4, weightChance=60},
+                {name="ArmyStorageGuns", min=0, max=99, weightChance=20},
+                {name="ArmyStorageGuns", min=1, max=4, weightChance=60},
+                {name="ArmyStorageMedical", min=1, max=4, weightChance=60},
+                {name="ArmyStorageOutfit", min=1, max=4, weightChance=60},
+                {name="ArmySurplusFootwear", min=1, max=2, weightChance=60},
+                {name="ArmySurplusMisc", min=1, max=2, weightChance=60},
+                {name="ArmySurplusMisc", min=0, max=1, weightChance=20},
+                {name="ArmyStorageGuns", min=1, max=2, weightChance=60},
+                {name="ArmyStorageGuns", min=1, max=2, weightChance=80},
+                {name="ArmyStorageGuns", min=0, max=2, weightChance=40},
+                {name="ArmyStorageElectronics", min=1, max=4, weightChance=60},
+                {name="ArmyStorageElectronics", min=0, max=2, weightChance=40},
+            }
+}
+
+SuburbsDistributions.armystorage.fridge = {
+            procedural = true,
+            procList = {
+                {name="ArmyStorageGuns", min=1, max=4, weightChance=60},
+                {name="ArmyStorageGuns", min=1, max=2, weightChance=40},
+                {name="ArmyStorageGuns", min=2, max=4, weightChance=60},
+                {name="ArmyStorageGuns", min=1, max=4, weightChance=60},
+                {name="ArmyStorageGuns", min=0, max=99, weightChance=20},
+                {name="ArmyStorageGuns", min=1, max=4, weightChance=60},
+                {name="ArmyStorageMedical", min=1, max=4, weightChance=60},
+                {name="ArmyStorageOutfit", min=1, max=4, weightChance=60},
+                {name="ArmySurplusFootwear", min=1, max=2, weightChance=60},
+                {name="ArmySurplusMisc", min=1, max=2, weightChance=60},
+                {name="ArmySurplusMisc", min=0, max=1, weightChance=20},
+                {name="ArmyStorageGuns", min=1, max=2, weightChance=60},
+                {name="ArmyStorageGuns", min=1, max=2, weightChance=80},
+                {name="ArmyStorageGuns", min=0, max=2, weightChance=40},
+                {name="ArmyStorageElectronics", min=1, max=4, weightChance=60},
+                {name="ArmyStorageElectronics", min=0, max=2, weightChance=40},
+            }
+        }
+        
+SuburbsDistributions.armystorage.counter = {
+            procedural = true,
+            procList = {
+                {name="ArmyStorageGuns", min=1, max=4, weightChance=60},
+                {name="ArmyStorageGuns", min=1, max=2, weightChance=40},
+                {name="ArmyStorageGuns", min=2, max=4, weightChance=60},
+                {name="ArmyStorageGuns", min=1, max=4, weightChance=60},
+                {name="ArmyStorageGuns", min=0, max=99, weightChance=20},
+                {name="ArmyStorageGuns", min=1, max=4, weightChance=60},
+                {name="ArmyStorageMedical", min=1, max=4, weightChance=60},
+                {name="ArmyStorageOutfit", min=1, max=4, weightChance=60},
+                {name="ArmySurplusFootwear", min=1, max=2, weightChance=60},
+                {name="ArmySurplusMisc", min=1, max=2, weightChance=60},
+                {name="ArmySurplusMisc", min=0, max=1, weightChance=20},
+                {name="ArmyStorageGuns", min=1, max=2, weightChance=60},
+                {name="ArmyStorageGuns", min=1, max=2, weightChance=80},
+                {name="ArmyStorageGuns", min=0, max=2, weightChance=40},
+                {name="ArmyStorageElectronics", min=1, max=4, weightChance=60},
+                {name="ArmyStorageElectronics", min=0, max=2, weightChance=40},
+            }
+        }
