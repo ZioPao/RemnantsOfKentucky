ff28a5f4617363c4e5ebbad6d9ba1fcfbd166a77
ZioPao <picchetti1998@gmail.com>
2024-03-02 23:47:25 +0100

    Hotfix to sledgehammers

--- media/lua/client/ROK/ClientOverrides.lua
+++ media/lua/client/ROK/ClientOverrides.lua
@@ -10,8 +10,25 @@ end
 Events.EveryOneMinute.Add(ResetBoredomAndUnhappyness)
 
 
---* DISABLE SCRAPPING THROUGH CONTEXT MENU WHILE IN STATIC ROOM
+
 local SafehouseInstanceHandler = require("ROK/SafehouseInstanceHandler")
+
+
+--* DISABLE SLEDGEHAMMERS IN SAFEHOUSE TO PREVENT FUCKY THINGS
+local og_ISDestroyStuffActionIsValid = ISDestroyStuffAction.isValid
+
+function ISDestroyStuffAction:isValid()
+    local ogReturn = og_ISDestroyStuffActionIsValid(self)
+
+    if SafehouseInstanceHandler.IsInSafehouse() then
+        return false
+    end
+
+    return ogReturn
+end
+
+
+--* DISABLE SCRAPPING THROUGH CONTEXT MENU WHILE IN STATIC ROOM
 
 
 local og_ISMoveablesActionIsValid = ISMoveablesAction.isValid
