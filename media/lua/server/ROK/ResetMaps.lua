661b1c358b16ad45faf5f4cdb0b6906bd7be9d5c
ZioPao <picchetti1998@gmail.com>
2024-03-03 17:31:46 +0100

    Working Map resetter without having to mess with python scripts :))

--- media/lua/server/ROK/ResetMaps.lua
+++ media/lua/server/ROK/ResetMaps.lua
@@ -1,3 +1,7 @@
--- if isServer() then
---     manipulateSavefile("map_t\\.bin", "DeleteInstancesEFT")
--- end
+Events.OnServerStarted.Add(function()
+    pcall(function()
+        debugPrint("Deleting maps data on start")
+        local path = "Multiplayer\\" .. getServerName()
+        deleteInstancesEFT(path) -- Java mod necessary for this to work
+    end)
+end)
