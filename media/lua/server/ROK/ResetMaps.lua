417afc934be17d4810445f0eb7b89939aef013b7
ZioPao <picchetti1998@gmail.com>
2024-03-03 18:46:28 +0100

    Added reset to PVPInstances after deleting save files

--- media/lua/server/ROK/ResetMaps.lua
+++ media/lua/server/ROK/ResetMaps.lua
@@ -3,5 +3,9 @@ Events.OnServerStarted.Add(function()
         debugPrint("Deleting maps data on start")
         local path = "Multiplayer\\" .. getServerName()
         deleteInstancesEFT(path) -- Java mod necessary for this to work
+
+        -- Reset Used Instances in ModData
+        local PvpInstanceManager = require("ROK/PvpInstanceManager")
+        PvpInstanceManager.Reset()
     end)
 end)
