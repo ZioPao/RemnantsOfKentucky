fbece64337fd3744eacb287ca53c1df075e05a00
ZioPao <picchetti1998@gmail.com>
2024-04-15 14:44:49 +0200

    Added some failsafes

--- media/lua/server/ROK/ResetMaps.lua
+++ media/lua/server/ROK/ResetMaps.lua
@@ -1,22 +1,24 @@
-Events.OnServerStarted.Add(function()
-    pcall(function()
-        if deleteInstancesROK then
-            debugPrint("Deleting maps data on start")
-            local path = "Multiplayer\\" .. getServerName()
-            
-            local startX = 0
-            local startY = 0
+if isServer() then
+    Events.OnServerStarted.Add(function()
+        pcall(function()
+            if deleteInstancesROK then
+                debugPrint("Deleting maps data on start")
+                --local path = "Multiplayer\\" .. getServerName()
 
-            local endX = 600
-            local endY = 100
+                local startX = 0
+                local startY = 0
 
-            deleteInstancesROK(path, startX, startY, endX, endY)-- Java mod necessary for this to work
+                local endX = 600
+                local endY = 100
 
-            -- Reset Used Instances in ModData
-            local PvpInstanceManager = require("ROK/PvpInstanceManager")
-            PvpInstanceManager.Reset()
-        else
-            debugPrint("JAVA MOD NOT FOUND! PVP INSTANCES DATA WON'T BE RESET")
-        end
+                deleteInstancesROK(startX, startY, endX, endY) -- Java mod necessary for this to work
+
+                -- Reset Used Instances in ModData
+                local PvpInstanceManager = require("ROK/PvpInstanceManager")
+                PvpInstanceManager.Reset()
+            else
+                debugPrint("JAVA MOD NOT FOUND! PVP INSTANCES DATA WON'T BE RESET")
+            end
+        end)
     end)
-end)
+end
