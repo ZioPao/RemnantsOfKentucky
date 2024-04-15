45e06d98bcb7ec84f9f52111ed1ba4f711f97a65
ZioPao <picchetti1998@gmail.com>
2024-04-15 10:35:25 +0200

    Re-enabled support for Java mod (automatic pvp instances reset)

--- media/lua/server/ROK/ResetMaps.lua
+++ media/lua/server/ROK/ResetMaps.lua
@@ -1,12 +1,15 @@
--- Events.OnServerStarted.Add(function()
---     -- TODO Re-enable it
---     pcall(function()
---         -- debugPrint("Deleting maps data on start")
---         -- local path = "Multiplayer\\" .. getServerName()
---         -- deleteInstancesEFT(path) -- Java mod necessary for this to work
+Events.OnServerStarted.Add(function()
+    pcall(function()
+        if deleteInstancesEFT then
+            debugPrint("Deleting maps data on start")
+            local path = "Multiplayer\\" .. getServerName()
+            deleteInstancesEFT(path) -- Java mod necessary for this to work
 
---         -- -- Reset Used Instances in ModData
---         -- local PvpInstanceManager = require("ROK/PvpInstanceManager")
---         -- PvpInstanceManager.Reset()
---     end)
--- end)
+            -- Reset Used Instances in ModData
+            local PvpInstanceManager = require("ROK/PvpInstanceManager")
+            PvpInstanceManager.Reset()
+        else
+            debugPrint("JAVA MOD NOT FOUND! PVP INSTANCES DATA WON'T BE RESET")
+        end
+    end)
+end)
